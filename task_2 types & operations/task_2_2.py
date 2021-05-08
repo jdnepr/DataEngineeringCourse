5aee58eb08695061c1b726e5637803112f149fa8
YUVOLGA <yuliya.volga@daimler.com>
2021-05-08 12:25:37 +0200

    taks 2 completed

--- task_2 types & operations/task_2_2.py
+++ task_2 types & operations/task_2_2.py
@@ -1,32 +1,34 @@
-list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-
-# add '0' to all number less than 10, without +
-#for i in range(len(list)):
-#    if list[i].isnumeric():
-#        if not int(list[i])//10:
-#            list[i] = "0" + list[i]
-
-print(list)
+sentence = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 
 i = 0
-while i < len(list):
+# used while loop, cause otherwise I cannot alter iterator variable - i
+while i < len(sentence):
     numeric_part = ""
     not_numeric_part = ""
-    for j in list[i]:
+    for j in sentence[i]: # save numeric and non-numeric parts of an element in separate variables
         if j.isnumeric():
             numeric_part += j
         else:
             not_numeric_part += j
-    if numeric_part and (not int(numeric_part)//10):
-        list[i] = not_numeric_part + "0" + numeric_part
+    if numeric_part and (not int(numeric_part)//10): #
+        sentence[i] = not_numeric_part + "0" + numeric_part
 
     if numeric_part:
-        list.insert(i, '\"')
-        list.insert(i + 2, '\"')
+        sentence.insert(i, '\"')
+        sentence.insert(i + 2, '\"')
         i = i + 2
 
     i = i + 1 # while loop increment
+print(sentence)
 
-print(list)
-message = ' '.join(list)
+message = ''
+i = 0
+while i < len(sentence):
+    if sentence[i] == '\"':
+        message += f'{sentence[i]}{sentence[i + 1]}{sentence[i + 2]} '
+        i += 2
+    else:
+        message += sentence[i] + ' '
+    i += 1
+
 print(message)
