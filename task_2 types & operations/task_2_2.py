7f702861ed5e82b2f90b713bc0fe232fa6ca199e
YUVOLGA <yuliya.volga@daimler.com>
2021-05-08 12:02:47 +0200

    task 4 complete, task 2 90%, needs revision

--- task_2 types & operations/task_2_2.py
+++ task_2 types & operations/task_2_2.py
@@ -1,22 +1,32 @@
 list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 
 # add '0' to all number less than 10, without +
-for i in range(len(list)):
-    if list[i].isnumeric():
-        if not int(list[i])//10:
-            list[i] = "0" + list[i]
+#for i in range(len(list)):
+#    if list[i].isnumeric():
+#        if not int(list[i])//10:
+#            list[i] = "0" + list[i]
 
 print(list)
 
-for i in range(len(list)):
+i = 0
+while i < len(list):
+    numeric_part = ""
+    not_numeric_part = ""
     for j in list[i]:
-        numeric_part = ''
-        not_numeric_part = ''
         if j.isnumeric():
             numeric_part += j
         else:
             not_numeric_part += j
+    if numeric_part and (not int(numeric_part)//10):
+        list[i] = not_numeric_part + "0" + numeric_part
 
-    list[i] = not_numeric_part + "0" + numeric_part
+    if numeric_part:
+        list.insert(i, '\"')
+        list.insert(i + 2, '\"')
+        i = i + 2
+
+    i = i + 1 # while loop increment
 
 print(list)
+message = ' '.join(list)
+print(message)
